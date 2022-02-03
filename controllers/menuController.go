092855c14eb66453bc3f8df81ef3368a5af74815
abdulaleem-git <zah7131@gmail.com>
2022-02-02 23:38:47 -0800

    Worked on getfoods, getmenu, and createmenu apis.

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -2,13 +2,16 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/abdulaleem-git/restaurant-mgmt-system/database"
+	"github.com/abdulaleem-git/restaurant-mgmt-system/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -32,10 +35,50 @@ func GetMenus() gin.HandlerFunc {
 	}
 }
 func GetMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var menu_id = c.Param("menu_id")
+		var menu models.Menu
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": menu_id}).Decode(&menu)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error while listing menu"})
+			return
+		}
+		c.JSON(http.StatusOK, menu)
+	}
 }
 func CreateMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+
+		if err := c.BindJSON(&menu); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(menu)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		menu.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		menu.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		menu.ID = primitive.NewObjectID()
+		menu.Menu_id = menu.ID.Hex()
+
+		result, insertErr := menuCollection.InsertOne(ctx, menu)
+
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": fmt.Sprintf("food item is not created")})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, result)
+
+	}
 }
 func UpdateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {}
